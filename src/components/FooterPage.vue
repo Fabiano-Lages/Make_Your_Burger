e20b482ff07b23e142cac0cd9a47766aacec1bee
Fabiano Lages <fabianolages@gmail.com>
2024-07-07 01:04:38 -0300

    Início e preparo do layout

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+        <p>Make Your Burger &copy; 2024</p>
+    </footer>
+</template>
+<script>
+    export default {
+        name: 'FooterPage'
+    }
+</script>
+<style scoped>
+    footer {
+        position: absolute;
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        bottom: 0;
+    }
+</style>
